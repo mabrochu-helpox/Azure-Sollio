640babc9464f3b102f2eefbe3091621ec5d60508
mabrochu <69210101+mabrochu-helpox@users.noreply.github.com>
2022-04-14 19:18:42 -0400

    Update 01_Install-LanguagePack-French.ps1

--- Sollio - Azure Image Builder/01_Install-LanguagePack-French.ps1
+++ Sollio - Azure Image Builder/01_Install-LanguagePack-French.ps1
@@ -91,6 +91,32 @@ if ($CurentLanguage -ne "fr-CA" )
         Add-WindowsPackage -Online -PackagePath $LIPContent\Microsoft-Windows-StepsRecorder-Package~31bf3856ad364e35~amd64~fr-ca~.cab
         Add-WindowsPackage -Online -PackagePath $LIPContent\Microsoft-Windows-WordPad-FoD-Package~31bf3856ad364e35~amd64~fr-ca~.cab
 
+
+        ##Set Inbox App Package Content Stores##
+        [string] $AppsContent = "C:\HelpOX\GoldenImage\LanguagePack\21H2-fr-ca\"
+
+        ##Update installed Inbox Store Apps##
+        foreach ($App in (Get-AppxProvisionedPackage -Online)) {
+	        $AppPath = $AppsContent + $App.DisplayName + '_' + $App.PublisherId
+	        Write-Host "Handling $AppPath"
+	        $licFile = Get-Item $AppPath*.xml
+	        if ($licFile.Count) {
+		        $lic = $true
+		        $licFilePath = $licFile.FullName
+	        } else {
+		        $lic = $false
+	        }
+	        $appxFile = Get-Item $AppPath*.appx*
+	        if ($appxFile.Count) {
+		        $appxFilePath = $appxFile.FullName
+		        if ($lic) {
+			        Add-AppxProvisionedPackage -Online -PackagePath $appxFilePath -LicensePath $licFilePath 
+		        } else {
+			        Add-AppxProvisionedPackage -Online -PackagePath $appxFilePath -skiplicense
+		        }
+	        }
+        }
+
     }
 
     catch {
