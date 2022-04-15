1456a6722d3012b8f4d5dfbeeab4d32be4a40640
mabrochu <69210101+mabrochu-helpox@users.noreply.github.com>
2022-04-15 12:24:20 -0400

    Update 08_Install-CutePDF.ps1

--- Sollio - Azure Image Builder/08_Install-CutePDF.ps1
+++ Sollio - Azure Image Builder/08_Install-CutePDF.ps1
@@ -6,9 +6,14 @@ Write-Host -ForegroundColor Green "[HelpOX] Beginning of Prod-WVD node configura
 Set-ExecutionPolicy Unrestricted -Force
 
 New-Item -Path "C:\HelpOX\GoldenImage\Log" -ItemType directory -force
-New-Item -Path "C:\HelpOX\GoldenImage\Log\$env:computername.txt" -ItemType file -force
 $logpath = "C:\HelpOX\GoldenImage\Log"
+$LogFile = "C:\HelpOX\GoldenImage\Log\$env:computername.txt"
 
+if (!(Test-Path $LogFile)) {
+    write-host 'Creation du fichier de log'
+    New-Item -Path "C:\HelpOX\GoldenImage\Log\$env:computername.txt" -ItemType file -force
+
+}
 
 ########################################################
 ## Install CutePDF Writer                             ##
@@ -19,12 +24,23 @@ $path = "C:\Program Files (x86)\CutePDF Writer\CutePDFWriter.exe"
 if (!(Test-Path $path)) {
   
     try{
+        Add-Content -Path $LogFile "========================== Installation De CutePDF =========================="
+
+        $now = Get-Date -Format "MM/dd/yyyy HH:mm"
+        Add-Content -Path $LogFile "[$now] Telechargement de CutePDF en cours ..."
+        
         Write-Host -ForegroundColor yellow "[HelpOX] Installing CutePDF Writer in progress..."
         New-Item -Path "c:\" -Name "temp" -ItemType "directory"
         Invoke-WebRequest -Uri 'https://sollioazureimagebuilder.blob.core.windows.net/sollioazureimagebuilder/CuteWriter.exe' -OutFile 'C:\temp\CuteWriter.exe'
+        $now = Get-Date -Format "MM/dd/yyyy HH:mm"
+        Add-Content -Path $LogFile "[$now] Telechargement de CutePDF terminer"
+        Add-Content -Path $LogFile "[$now] Installation de CutePDF en cours ..."
         C:\temp\CuteWriter.exe /VERYSILENT /NORESTART
-        Start-sleep -Seconds 60
+        Start-sleep -Seconds 90
+        $now = Get-Date -Format "MM/dd/yyyy HH:mm"
+        Add-Content -Path $LogFile "[$now] Installation de CutePDF terminer"
         Remove-Item "C:\temp" -Force -Recurse -Confirm:$false
+        Add-Content -Path $LogFile "[$now] Nettoyage des sources de CutePDF terminer"
 
   }
     catch {
