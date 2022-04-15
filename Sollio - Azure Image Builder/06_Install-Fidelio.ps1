e2383b0648cd4b7e8957eafcb92a0845d5497f0a
mabrochu <69210101+mabrochu-helpox@users.noreply.github.com>
2022-04-15 12:16:22 -0400

    Update 06_Install-Fidelio.ps1

--- Sollio - Azure Image Builder/06_Install-Fidelio.ps1
+++ Sollio - Azure Image Builder/06_Install-Fidelio.ps1
@@ -19,8 +19,14 @@ Write-Host -ForegroundColor Green "[HelpOX] Beginning of Prod-WVD node configura
 Set-ExecutionPolicy Unrestricted -Force
 
 New-Item -Path "C:\HelpOX\GoldenImage\Log" -ItemType directory -force
-New-Item -Path "C:\HelpOX\GoldenImage\Log\$env:computername.txt" -ItemType file -force
 $logpath = "C:\HelpOX\GoldenImage\Log"
+$LogFile = "C:\HelpOX\GoldenImage\Log\$env:computername.txt"
+
+if (!(Test-Path $LogFile)) {
+    write-host 'Creation du fichier de log'
+    New-Item -Path "C:\HelpOX\GoldenImage\Log\$env:computername.txt" -ItemType file -force
+
+}
 
 ########################################################
 ## Install de Commsoft Fidelio                        ##
@@ -29,14 +35,32 @@ $logpath = "C:\HelpOX\GoldenImage\Log"
 if (-not (Get-WmiObject win32_product | where{$_.Name -like "*Fidelio*"}))
 {
     try {
-        Write-Host -ForegroundColor yellow "[HelpOX] Installing Commsoft Fidelio..."
-        New-Item -Path "c:\" -Name "temp" -ItemType "directory"
-        Invoke-WebRequest -Uri 'https://sollioazureimagebuilder.blob.core.windows.net/sollioazureimagebuilder/FidelioSetup.msi' -OutFile 'C:\temp\FidelioSetup.msi'
-        msiexec /quiet /passive /qn /i "C:\temp\FidelioSetup.msi"
-        Start-sleep -Seconds 60
-        Remove-Item "C:\Program Files (x86)\Commsoft Technologies Inc" -Force -Recurse -Confirm:$false
-        Remove-Item C:\Users\Public\Desktop\* -Force -Confirm:$false
-        Remove-Item "C:\temp" -Force -Recurse -Confirm:$false
+         Add-Content -Path $LogFile "========================== Installation de Commsoft Fidelio =========================="
+
+         $now = Get-Date -Format "MM/dd/yyyy HH:mm"
+         Add-Content -Path $LogFile "[$now] Telechargement de Fidelio en cours ..."
+         
+         Write-Host -ForegroundColor yellow "[HelpOX] Installing Commsoft Fidelio..."
+         New-Item -Path "c:\" -Name "temp" -ItemType "directory"
+         Invoke-WebRequest -Uri 'https://sollioazureimagebuilder.blob.core.windows.net/sollioazureimagebuilder/FidelioSetup.msi' -OutFile 'C:\temp\FidelioSetup.msi'
+         
+         $now = Get-Date -Format "MM/dd/yyyy HH:mm"
+         Add-Content -Path $LogFile "[$now] Telechargement de Fidelio terminer" 
+         Add-Content -Path $LogFile "[$now] Installation de Fidelio en cour ..." 
+         
+         msiexec /quiet /passive /qn /i "C:\temp\FidelioSetup.msi"
+         Start-sleep -Seconds 90
+
+         $now = Get-Date -Format "MM/dd/yyyy HH:mm"
+         Add-Content -Path $LogFile "[$now] Installation de Fidelio terminer" 
+
+         Remove-Item "C:\Program Files (x86)\Commsoft Technologies Inc" -Force -Recurse -Confirm:$false
+         Remove-Item C:\Users\Public\Desktop\* -Force -Confirm:$false
+         Remove-Item "C:\temp" -Force -Recurse -Confirm:$false
+         
+         $now = Get-Date -Format "MM/dd/yyyy HH:mm"
+         Add-Content -Path $LogFile "[$now] Nettoyage des sources de Fidelio terminer" 
+         Add-Content -Path $LogFile "========================== Installation de Commsoft Fidelio =========================="
     }
 
     catch {
