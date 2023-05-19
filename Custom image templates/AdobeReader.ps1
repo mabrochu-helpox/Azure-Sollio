c8caafe6b1f5bdec196a44b36a2e78525102be4f
mabrochu <69210101+mabrochu-helpox@users.noreply.github.com>
2023-05-19 09:52:03 -0400

    Add files via upload

--- Custom image templates/AdobeReader.ps1
+++ Custom image templates/AdobeReader.ps1
@@ -8,7 +8,7 @@
 .DESCRIPTION
    Installation silencieuse de Adobe Reader
 .NOTES
-   Le scipt s'execute en mode silent et créer un log dans 
+   Le scipt s'execute en mode silent
 .AUTHOR
    Marc-André Brochu / 514-666-4357 Ext:3511 / mabrochu@it-ed.com
 .VERSION
@@ -28,15 +28,16 @@ $logpath = "C:\ited\GoldenImage\Log"
 $Log_File = "C:\ited\GoldenImage\Log\$env:computername.txt"
 $now = Get-Date -Format "MM/dd/yyyy HH:mm"
 
-if (-not(Test-Path -Path $Log_File  -PathType Leaf)) {
-    Write-Host "Le fichier de log est indisponible. Création du fichier de log"
+if (-not(Test-Path -Path $Log_File -PathType Leaf)) {
+    Write-Host "[ited] - Le fichier de log est indisponible. Création du fichier de log"
     New-Item -path C:\ited -ItemType Directory | Out-Null -ErrorAction SilentlyContinue
     New-Item -path $Log_File -ItemType File | Out-Null -ErrorAction SilentlyContinue
 }
 try{
     Add-Content -Path $Log_File "========================== Installation de Adobe Reader =========================="
     $now = Get-Date -Format "MM/dd/yyyy HH:mm"
-    Add-Content -Path $Log_File "[$now] Installing Foxit Reader in progress..."
+    Add-Content -Path $Log_File "[$now] Installing Adobe Reader in progress..."
+    write-host "[ited] - Installation de Adobe Reader en cours ..."
     New-Item -Path "c:\" -Name "temp" -ItemType "directory"
     Invoke-WebRequest -Uri 'https://sollioazureimagebuilder.blob.core.windows.net/sollioazureimagebuilder/AcroRdrDC2200120117_fr_FR.exe' -OutFile 'C:\temp\AcroRdrDC2200120117_fr_FR.exe'
     C:\temp\AcroRdrDC2200120117_fr_FR.exe /sAll /rs /msi EULA_ACCEPT=YES
@@ -44,7 +45,7 @@ try{
     #Remove-Item "C:\temp" -Force -Recurse -Confirm:$false
     $now = Get-Date -Format "MM/dd/yyyy HH:mm"
     Add-Content -Path $Log_File "[$now] Adobe Reader installed successfully"
-    write-host "ited - Installation de Adobe Reader success"
+    write-host "[ited] - Installation de Adobe Reader success"
 }
 catch {
         Write-Error $_
